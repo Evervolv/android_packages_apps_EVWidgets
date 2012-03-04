da2142172eff1caeca303f69674c3a0d018fde91
andrew.boren <andrew.boren@gmail.com>
2012-03-04 11:51:44 -0700

    Remove progaurd & get rid of unnecessary dependencies.

--- Android.mk
+++ Android.mk
@@ -1,17 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_JAVA_LIBRARIES := bouncycastle
-LOCAL_STATIC_JAVA_LIBRARIES := guava
-
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := EVWidgets
 LOCAL_CERTIFICATE := platform
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
